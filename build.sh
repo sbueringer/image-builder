9bcdab3ef4b9227f4a3238f5d4adc1819c746eed
Stefan Bueringer <sbueringer@gmail.com>
2020-03-01 17:47:55 +0100

    add pipeline

--- build.sh
+++ build.sh
@@ -8,9 +8,6 @@ DATE=$(date +%Y%m%d-%H%M)
 UBUNTU_VERSION="1910"
 K8S_VERSION="v1.16.2"
 
-git clone -b caas-1910 https://github.com/sbueringer/image-builder.git /tmp/image-builder
-cd /tmp/image-builder/images/capi
-
 SHORT_SHA="$(git rev-parse --short HEAD)"
 MAKE_VERSION="build-qemu-ubuntu-${UBUNTU_VERSION}"
 BUILD_VERSION="${UBUNTU_VERSION}-kube-${SHORT_SHA}-${DATE}"
