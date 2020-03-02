e987b09b95ecce57d4ce708463e0e96e968d6f2e
Stefan Bueringer <sbueringer@gmail.com>
2020-03-02 18:36:54 +0100

    .github/workflows

---
+++ .github/workflows/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+DATE=$(date +%Y%m%d-%H%M)
+
+UBUNTU_VERSION="2004"
+K8S_VERSION="v1.17.3"
+
+SHORT_SHA="$(git rev-parse --short HEAD)"
+MAKE_VERSION="build-qemu-ubuntu-${UBUNTU_VERSION}"
+BUILD_VERSION="${UBUNTU_VERSION}-kube-${SHORT_SHA}-${DATE}"
+
+echo "building image ubuntu-$BUILD_VERSION''"
+
+export PACKER_FLAGS="-debug -var 'accelerator=none' -var 'cpus=2' -var 'disk_size=10240' -var 'memory=4096'"
+export PACKER_LOG=1
+export PACKER_LOG_PATH=/tmp/packer.log
+tail -F ${PACKER_LOG_PATH} &
+
+cd ./images/capi
+
+make "${MAKE_VERSION}"
+
+ls -la ./output/ubuntu-${UBUNTU_VERSION}-kube-${K8S_VERSION}/
+
+echo -e "[INFO] Version of image: ${BUILD_VERSION}\n"
