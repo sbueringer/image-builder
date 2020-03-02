c5effcdb4d100d9e109c307d5ad20e1eaabf5b19
Stefan Bueringer <sbueringer@gmail.com>
2020-03-02 18:39:22 +0100

    add qemu build

---
+++ images/capi/hack/image-build-vmdk-optimize-qcow2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+BUILD_DIR=$1
+
+echo "Converting qcow2 to streamOptimized vmdk"
+qemu-img convert -f qcow2 -O vmdk -o subformat=streamOptimized ${BUILD_DIR}/qemu-kube-v1.16.2 ${BUILD_DIR}/qemu-kube-v1.16.2.vmdk
+
+echo "Compressing qcow2"
+qemu-img convert -f qcow2 -O qcow2 -c ${BUILD_DIR}/qemu-kube-v1.16.2 ${BUILD_DIR}/qemu-kube-v1.16.2.qcow2
