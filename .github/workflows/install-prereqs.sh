fa02299747d7f7445912a91a838b3a2eaa4648c5
Stefan Bueringer <sbueringer@gmail.com>
2020-03-12 19:56:14 +0100

    fixup

--- .github/workflows/install-prereqs.sh
+++ .github/workflows/install-prereqs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-packer_version=1.4.3
+packer_version=1.5.4
 
 sudo apt update && sudo apt-get install -y \
     unzip \
@@ -14,6 +14,7 @@ sudo apt update && sudo apt-get install -y \
     jq \
     rsync
 
+https://releases.hashicorp.com/packer/1.5.4/packer_1.5.4_linux_amd64.zip
 wget --quiet -O packer.zip https://releases.hashicorp.com/packer/${packer_version}/packer_${packer_version}_linux_amd64.zip \
  && unzip packer.zip \
  && rm packer.zip \
